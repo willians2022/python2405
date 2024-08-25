29d4e5839296af0737286574bf47e43b9064667a
willians2022 <willians_zno@hotmail.com>
2024-08-25 19:31:13 -0300

    manipulação de string

---
+++ codigos manipulação de string/comandos.py
@@ -0,0 +1,22 @@
+texto = " curso de python "
+#imprimir a variavel
+print(texto)
+
+#imprimindo texto por posição
+print(texto[-1])
+
+# mostra a ultima letra do texto nesse caso o espaço
+print(texto[-2])
+
+#mostra a penultima letra do texto nesse caso o "n"
+print(texto[9:15])
+
+#apartir da 10 posição  até a posição 16, pulando de dois em dois
+print(texto[9:15:2])
+
+#do  inicio até a 9 posição
+print(texto[:8])
+
+#da 9 posição até a ultima
+print(texto[8:])
+
